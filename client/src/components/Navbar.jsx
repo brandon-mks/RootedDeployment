16b749dbda2c4bd5823d988d5533e04d80aaf4ac
Sara <s.cristal.p@gmail.com>
2026-08-06 19:53:02 +0200

    navbar

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+function Navbar() {
+  return (
+    <nav className="site-nav" aria-label="Main navigation">
+      <a href="#about">About Us</a>
+      <a href="#contact">Contact</a>
+      <a href="/login">Log in</a>
+    </nav>
+  );
+}
+
+export default Navbar;
