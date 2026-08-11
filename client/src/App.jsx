8feecc871a0b5b7529793ea49b6dc9d5867b706f
Sara <s.cristal.p@gmail.com>
2026-08-11 17:10:06 +0200

    remove dom from import on app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router";
 
 import LandingPage from "./components/LandingPage.jsx";
 import DiscoverPage from "./components/DiscoverPage.jsx";
