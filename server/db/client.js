5155dd80b963c29dec570781190e20813a3d4d64
Sara Peña <73671315+saracpena@users.noreply.github.com>
2026-08-12 19:43:36 +0200

    Improve API error handling and local setup (#7)
    * update client services places
    
    * Improve API handling and local setup documentation

--- server/db/client.js
+++ server/db/client.js
@@ -1,3 +1,3 @@
 import pg from "pg";
-const client = new pg.Client(process.env.DATABASE_URL || "postgres://localhost/rooted_database");
+const client = new pg.Client(process.env.DATABASE_URL || "postgres://localhost/rooted");
 export default client;
