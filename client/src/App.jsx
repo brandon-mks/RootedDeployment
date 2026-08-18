9f16c65eb85aa8c3c78ea47d5d116547e1ece4de
Sara <s.cristal.p@gmail.com>
2026-08-18 17:28:12 +0200

    Reapply "Add Connect page with community filters" (#17)
    This reverts commit f0daa7d6a024d47fabd2dd18cc35734fc2fc112e.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,13 +6,15 @@ import LoginPage from "./pages/LoginPage";
 import AboutPage from "./pages/AboutPage";
 
 import DiscoverPage from "./components/DiscoverPage.jsx";
+import ConnectPage from "./pages/ConnectPage.jsx";
 import Chatbot from "./components/chatbot/Chatbot.jsx";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
-      <Route path="/discover" element={<DiscoverPage />} />
+      <Route path="/discover" element={<DiscoverPage />}/>
+      <Route path="/connect" element={<ConnectPage />} />
       <Route path="/chat" element={<Chatbot />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/about" element={<AboutPage />} />
